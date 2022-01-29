47c69d8915a93c8682e0729f586847c444ecbe49
Kunal Patil <91950357+kunal-Patill@users.noreply.github.com>
2022-01-29 12:28:10 +0530

    Update array001122.java

--- placementsArray/array001122.java
+++ placementsArray/array001122.java
@@ -1,3 +1,4 @@
+// Here we will use the inbuilt function i.e. Arrays.sort() which would directly sort the given array.
 package placementsArray;
 
 import java.util.Arrays;
@@ -8,11 +9,9 @@ public class array001122 {
 		
 		int a [] = {1,0,2,0,0,1,2,2,1,0};
 		
-//        Arrays.sort(a);
-//        
-//        System.out.printf("sorted array is :" +Arrays.toString(a));
-		
-		   
-        
+          Arrays.sort(a);
+          
+          System.out.printf("sorted array is :" +Arrays.toString(a));
 	}
 }
+// Arrays.toString() function helps us to print the array by converting it to a string .
