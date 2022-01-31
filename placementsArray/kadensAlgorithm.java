629ae40dee47d7c595f5831a3878a276b63154a8
Kunal Patil <91950357+kunal-Patill@users.noreply.github.com>
2022-01-31 12:38:56 +0530

    Update kadensAlgorithm.java

--- placementsArray/kadensAlgorithm.java
+++ placementsArray/kadensAlgorithm.java
@@ -1,10 +1,10 @@
-// Kadens Algorithm is the best optimum solution to find the max sum subarray in the gicen array 
+// Kadens Algorithm is the best optimum solution to find the maximum sum subarray in the given array with the best time complaxity that can be possible !!
 package placementsArray;
 
 public class kadensAlgorithm {
 
 	public static void main(String[] args) {
-		
+// Initialize two variables i.e. maxsum and currentsum to zero and follow the given process 		
 		int a[] = {5,-4,-2,6,-1,8};
 		int maxsum = 0;
 		int cursum = 0;
