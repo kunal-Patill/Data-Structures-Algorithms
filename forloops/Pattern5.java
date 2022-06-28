8c29fb5ee7871114d3aa03b8ab4d4c9d8087c2c2
Kunal Patil <91950357+kunal-Patill@users.noreply.github.com>
2022-06-28 18:31:05 +0530

    Create Pattern5.java

---
+++ forloops/Pattern5.java
@@ -0,0 +1,18 @@
+public static void main(String[]args){
+  Scanner sc= new Scanner(System.in);
+  int n = sc.nextInt();
+  for(int i=0;i<n;i++) {
+			System.out.println();
+			for(int j=n;j>i;j--) {
+				System.out.print("* ");
+			}
+		}
+  }
+
+
+// The above program generates the following pattern :
+// * * * * * 
+// * * * * 
+// * * * 
+// * * 
+// * 
