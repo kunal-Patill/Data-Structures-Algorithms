ba05bb8763677452e57a6edcbac0211b50b320c2
Kunal Patil <91950357+kunal-Patill@users.noreply.github.com>
2022-07-04 18:55:36 +0530

    Update pattern6.java

--- forloops/pattern6.java
+++ forloops/pattern6.java
@@ -1,12 +1,22 @@
+// This program helps to print a desired pattern :
+
 public static void main(String[]args) {
-	int n=5;
-	for(int i=1;i<=n;i++) {
-		for(int j=1;j<=n;j++) {
-			if(i==1 || j==1 || i==n || j==n)
-				System.out.print("1 ");
-			else
-				System.out.print("0 ");
-		}
-		System.out.println();
-	}
-	}
+int n=5;
+for(int i=1;i<=n;i++) {
+for(int j=1;j<=n;j++) {
+if(i==1 || j==1 || i==n || j==n)
+System.out.print("1 ");
+else
+System.out.print("0 ");
+}
+System.out.println();
+}
+}
+
+
+output for the given code is :
+1 1 1 1 1 
+1 0 0 0 1 
+1 0 0 0 1 
+1 0 0 0 1 
+1 1 1 1 1 
