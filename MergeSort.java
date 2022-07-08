5ec56bb442d59f87cea2ec1b51b01768fa2ae180
Kunal Patil <91950357+kunal-Patill@users.noreply.github.com>
2022-07-08 20:02:39 +0530

    Update MergeSort.java

--- MergeSort.java
+++ MergeSort.java
@@ -7,3 +7,41 @@ public static void mergeSort (int arr[], int l, int r) {
 			merge(arr, l, mid, r);
 		}
 	}
+
+public static void merge (int a[], int l, int mid, int r) {
+		int b[] = {};
+		int i=l;
+		int j=mid;
+		int k=l;
+		while(i<r) {
+			if(a[i]<a[j]) {
+				b[k] = a[i];
+				i++;
+			}
+			else {
+				b[k] =a[j];
+				j++;
+			}
+			k++;
+		}
+		if(i>mid) {
+			while(j<=r) {
+				b[k] = a[j];
+				j++;
+				k++;
+			}
+		}
+		else {
+			while(i<=mid) {
+				b[k] = a[i];
+				i++;
+				k++;
+			}
+		}
+		
+		for(int z=0; z<a.length; z++) {
+			a[z] = b[z];
+			z++;
+		}
+		
+	}
